cdf777bdde0195308d1a6a759f7dd3cea2d6316d
RameshwariVangar <rameshwarivangar@gmail.com>
2026-08-21 11:09:19 +0530

    newUpdate

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //export const BASE_URL = "https://connectify-yw2s.onrender.com" ;
 
-export const BASE_URL = "http://localhost:9090";
+export const BASE_URL = "https://connectify-yw2s.onrender.com";
 
 export const clientServer = axios.create({
     baseURL: BASE_URL 
